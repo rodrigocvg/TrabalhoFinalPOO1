8169c0c02ae4ed847021ffa3ac3cd156e291301c
rodrigocvg <rod300603@gmail.com>
2022-07-28 18:23:00 -0300

    construtorContas

--- src/Instituição/Contas/Corrente.java
+++ src/Instituição/Contas/Corrente.java
@@ -2,7 +2,36 @@ package Instituição.Contas;
 //import Personas.Data;
 import java.util.Date;
 
+import Personas.Data;
+
 public class Corrente extends Conta {
-    //nro da conta, saldo atual, data de abertura, data da última movimentação, limite do cheque especial e valor da taxa administrativa.
+   
+    // limite do cheque especial e valor da taxa administrativa.
+    private float limCheque;
+    private float taxAdmin;
+    public float getLimCheque() {
+        return limCheque;
+    }
+
+    public Corrente(String Nome, int CPF, int Num_Conta, int Senha_Conta, float saldo, int StatusDaConta,
+    boolean conjunta, Personas.Clientes.Clientes Cliente_primario, Personas.Clientes.Clientes[] Clientes,
+    Instituição.Agencia Agencia, Data Abertura_de_Conta, Data Ultima_Movimentacao, float limite, float taxAdmin) {
+    super(Nome, CPF, Num_Conta, Senha_Conta, saldo, StatusDaConta, conjunta, Cliente_primario, Clientes, Agencia,
+        Abertura_de_Conta, Ultima_Movimentacao);
+        this.limCheque = 0;
+        this.taxAdmin = 0;
+
+}
+
+    public void setLimCheque(float limCheque) {
+        this.limCheque = limCheque;
+    }
+    public float getTaxAdmin() {
+        return taxAdmin;
+    }
+    public void setTaxAdmin(float taxAdmin) {
+        this.taxAdmin = taxAdmin;
+    }
+
     
 }
--- src/Instituição/Contas/Poupanca.java
+++ src/Instituição/Contas/Poupanca.java
@@ -1,6 +1,27 @@
 package Instituição.Contas;
 
-public class Poupanca extends Conta {
-    // nro da conta, saldo atual, data de abertura, data da última movimentação, rendimento do mês atual.
+import Personas.Data;
 
+public class Poupanca extends Conta {
+   
+
+    // rendimento do mês atual.
+    private float rendimento;
+
+    public Poupanca(String Nome, int CPF, int Num_Conta, int Senha_Conta, float saldo, int StatusDaConta,
+    boolean conjunta, Personas.Clientes.Clientes Cliente_primario, Personas.Clientes.Clientes[] Clientes,
+    Instituição.Agencia Agencia, Data Abertura_de_Conta, Data Ultima_Movimentacao, float rendimento) {
+    super(Nome, CPF, Num_Conta, Senha_Conta, saldo, StatusDaConta, conjunta, Cliente_primario, Clientes, Agencia,
+        Abertura_de_Conta, Ultima_Movimentacao);
+    this.rendimento = rendimento;
+//TODO Auto-generated constructor stub
+}
+    public float getRendimento() {
+        return rendimento;
+    }
+
+    public void setRendimento(float rendimento) {
+        this.rendimento = rendimento;
+    }
+    
 }
