c9a743b777117407dc6b31db260f75320f67856d
rodrigocvg <rod300603@gmail.com>
2022-08-02 13:16:37 -0300

    .

--- src/Instituição/Contas/GerenciaArquivos.java
+++
@@ -1,9 +0,0 @@
-package Instituição.Contas;
-import java . io .*;
-
-public interface GerenciaArquivos {
-    
-
-    public void escrita(String nomeArq, GerenciaArquivos a);
-    public void leitura(String nomeArq, GerenciaArquivos a);
-}
