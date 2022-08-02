cbf6a3fd387c4a1bb10a9ede2accf7b12569b969
rodrigocvg <rod300603@gmail.com>
2022-08-02 16:38:11 -0300

    pode ser que funcione, mas ainda nao

--- src/Personas/Clientes/Clientes.java
+++ src/Personas/Clientes/Clientes.java
@@ -1,10 +1,19 @@
 package Personas.Clientes;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
 import Personas.Data;
 import Personas.Pessoa;
 import Personas.Endereco;
 
-public class Clientes extends Pessoa {
+public class Clientes extends Pessoa implements Serializable{
     private String Escolaridade;
     private int NumAgencia;
 
@@ -25,6 +34,46 @@ public class Clientes extends Pessoa {
         super(Nome, CPF, Data_de_Nascimento, Endereco, Sexo, Estado_Civil);
         this.Escolaridade = Escolaridade;
         this.NumAgencia = NumAgencia;
+    }
+
+    /* Pode ser que funcione, por enquanto não ta */
+    public void escrita(){
+        File arq = null;
+        ObjectOutputStream obj = null;
+        try {
+            arq = new File("e:/POO1/Trabalho/Clientes/testeArquivo.txt");
+            obj = new ObjectOutputStream(new FileOutputStream(arq,true));
+            obj.writeObject(obj);
+            obj.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public ArrayList<Clientes> ler(){
+        FileInputStream arquivo = null;
+        ArrayList<Clientes> clientes = new ArrayList<Clientes>();
+        Clientes cliente = null;
+        ObjectInputStream obj = null;
+        try {
+            arquivo = new FileInputStream("e:/POO1/Trabalho/src/Clientes/testeArquivo.txt");
+            obj = new ObjectInputStream(arquivo);
+            do {
+                cliente = (Clientes) obj.readObject();
+                if (cliente != null)
+                    clientes.add(cliente);
+           } while (cliente != null);
+            arquivo.close();
+            obj.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } 
+        return clientes;
+    }
+
+    public void leitura(){
+
     }
 
 
